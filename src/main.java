06d184d4feb68dcee03dee10b5d70b6cf7147d68
Rayene-Hannachi <rayene.hannachi@esprit.tn>
2025-10-03 09:40:18 +0100

    Prosit1

--- src/main.java
+++ src/main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
         ZooManagment zoo = new ZooManagment();
         Scanner name = new Scanner(System.in);
