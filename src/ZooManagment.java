73b653e1f4ea9328e02326d13f79af36461773aa
Rayene-Hannachi <rayene.hannachi@esprit.tn>
2025-10-03 08:58:37 +0100

    Prosit1

---
+++ src/ZooManagment.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ZooManagment {
+    private int nbrCages;
+    private String zooName;
+
+    public String getZooName() {
+        return zooName;
+    }
+
+    public int getNbrCages() {
+        return nbrCages;
+    }
+    public void setNbrCages(int nbrCages) {
+        this.nbrCages = nbrCages;
+    }
+
+    public void setZooName(String zooName) {
+        this.zooName = zooName;
+    }
+
+
+}
---
+++ src/main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        ZooManagment zoo = new ZooManagment();
+        Scanner name = new Scanner(System.in);
+        do {
+            System.out.println("Enter zoo name");
+            zoo.setZooName(name.nextLine());
+        }while(zoo.getZooName().trim().isEmpty());
+        Scanner cages = new Scanner(System.in);
+        do{
+            System.out.println("Enter nbrchages");
+            zoo.setNbrCages(cages.nextInt());
+        }while(zoo.getNbrCages()<0);
+        System.out.println(zoo.getZooName() + " comporte " + zoo.getNbrCages() + " cages");
+    }
+}
